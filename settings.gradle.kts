ca79703a48d9a15e731dad9435a4e90c935ce239
yongjunhong <yongjunh@apache.org>
2025-06-14 00:15:23 +0900

    update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,5 @@
 rootProject.name = "kotlin-pom-gradle"
 
-include(
-    "core:api",
-    "core:dsl",
-    "core:metadata",
-    "core:hierarchy",
-    "plugins:gradle-plugin",
-    "features:pom-generator",
-    "features:property-resolver",
-    "features:validation"
-)
-
 include("core")
 include("plugins")
 include("testing")
