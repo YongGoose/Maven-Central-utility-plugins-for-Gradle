74fef3c40d28430115ec04e2dd662112a855774c
yongjunhong <yongjunh@apache.org>
2025-07-11 13:59:23 +0900

    Add plugin descriptions

--- plugins/build.gradle.kts
+++ plugins/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm")
     id("java-gradle-plugin")
     `kotlin-dsl`
+    id("com.gradle.plugin-publish") version "1.2.1"
 }
 
 repositories {
@@ -22,20 +23,34 @@ kotlin {
 }
 
 gradlePlugin {
+    website = "https://github.com/YongGoose/kotlin-pom-gradle"
+    vcsUrl = "https://github.com/YongGoose/kotlin-pom-gradle.git"
+
     plugins {
-        create("organizationDefaultsProject") {
-            id = "io.github.yonggoose.organization-defaults-project"
-            implementationClass = "io.github.yonggoose.organizationdefaults.OrganizationDefaultsProjectPlugin"
-        }
+        plugins {
+            create("organizationDefaultsProject") {
+                id = "io.github.yonggoose.organization-defaults-project"
+                displayName = "Organization Defaults Project Plugin"
+                description = "A Gradle plugin to apply organization-wide defaults to projects."
+                tags = setOf("gradle", "plugin", "organization", "defaults")
+                implementationClass = "io.github.yonggoose.organizationdefaults.OrganizationDefaultsProjectPlugin"
+            }
 
-        create("organizationDefaultsSetting") {
-            id = "io.github.yonggoose.organization-defaults-setting"
-            implementationClass = "io.github.yonggoose.organizationdefaults.OrganizationDefaultsSettingsPlugin"
-        }
+            create("organizationDefaultsSetting") {
+                id = "io.github.yonggoose.organization-defaults-setting"
+                displayName = "Organization Defaults Settings Plugin"
+                description = "A Gradle plugin to apply organization-wide defaults to settings."
+                tags = setOf("gradle", "plugin", "organization", "defaults", "settings")
+                implementationClass = "io.github.yonggoose.organizationdefaults.OrganizationDefaultsSettingsPlugin"
+            }
 
-        create("artifactCheckProject") {
-            id = "io.github.yonggoose.organization-defaults-artifact-check-project"
-            implementationClass = "io.github.yonggoose.organizationdefaults.ArtifactCheckPluginForProject"
+            create("artifactCheckProject") {
+                id = "io.github.yonggoose.organization-defaults-artifact-check-project"
+                displayName = "Artifact Check Project Plugin"
+                description = "A Gradle plugin to check artifacts against organization defaults."
+                tags = setOf("gradle", "plugin", "organization", "defaults", "artifact", "check")
+                implementationClass = "io.github.yonggoose.organizationdefaults.ArtifactCheckPluginForProject"
+            }
         }
     }
 }
